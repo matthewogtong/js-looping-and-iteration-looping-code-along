1fa7063747517e73079923f82c934eae14f529ab
matthewogtong <ogtongm@gmail.com>
2020-12-10 20:43:33 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, string){
+   let newArray = []
+    for (let i = 0; i < array.length; i++){
+        let element = array[i]
+        newArray.push(`Thank you, ${element}, for the wonderful ${string} gift!`)
+    }
+    return newArray
+}
+
+function countDown(positiveInt){
+    while ( positiveInt > -1 ) {
+        console.log(positiveInt--)
+    }
+}
